678061313dcb5f6ffd46246eba0a2f1edb0d7eee
Upadhyay <prayag.upd@gmail.com>
2021-06-10 22:41:12 -0700

    upgrade postgres test container

--- src/test/java/com/testing/PostgresTest.java
+++ src/test/java/com/testing/PostgresTest.java
@@ -4,14 +4,15 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Rule;
+import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,9 +20,16 @@ public class PostgresTest {
 
     private final Set<HikariDataSource> datasourcesForCleanup = new HashSet<>();
 
-    @Rule
-    public GenericContainer postgresContainer = new GenericContainer<>("postgres:latest")
-            .withExposedPorts(5432);
+    @ClassRule
+    public static PostgreSQLContainer postgresContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("testdb")
+            .withUsername("sa")
+            .withPassword("sa")
+            .withExposedPorts(5555);
+
+    @Before
+    public void before() {
+    }
 
     @Test
     public void test() {
@@ -29,19 +37,16 @@ public class PostgresTest {
     }
 
     @Test
-    public void returnsSimpleData() throws SQLException {
-        try (PostgreSQLContainer postgres = new PostgreSQLContainer<>()) {
-            postgres.start();
+    public void returnsTodaysDate() throws SQLException {
 
-            DataSource ds = getDataSource(postgres);
-            var statement = ds.getConnection().createStatement();
-            var rs = statement.executeQuery("SELECT 1");
+        DataSource ds = getDataSource(postgresContainer);
+        var statement = ds.getConnection().createStatement();
+        var rs = statement.executeQuery("SELECT now()");
 
-            rs.next();
+        rs.next();
 
-            int resultSetInt = rs.getInt(1);
-            Assert.assertEquals("A basic SELECT query succeeds", 1, resultSetInt);
-        }
+        var now = rs.getDate(1);
+        Assert.assertEquals("NOW(): ", LocalDate.now().toString(), now.toString());
     }
 
     DataSource getDataSource(JdbcDatabaseContainer container) {
